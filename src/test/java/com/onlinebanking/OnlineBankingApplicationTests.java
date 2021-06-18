74e21d00de8bec168966f7c77e85d077e58b4f07
GeorgeAnguah <Georgeanguah@yahoo.com>
2021-06-18 16:19:17 -0400

    online-banking project setup

---
+++ src/test/java/com/onlinebanking/OnlineBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
