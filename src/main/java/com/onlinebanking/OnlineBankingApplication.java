561962c9ce2ac1262fe5772466c301173be2758a
Eric Opoku <eric.opoku20@yahoo.com>
2021-06-18 18:40:38 -0400

    #ob-w1wFWxTK - Configured code scanners for the project

--- src/main/java/com/onlinebanking/OnlineBankingApplication.java
+++ src/main/java/com/onlinebanking/OnlineBankingApplication.java
@@ -3,9 +3,21 @@ package com.onlinebanking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The class has the main method to get the application started.
+ *
+ * @author Eric Opoku
+ * @version 1.0
+ * @since 1.0
+ */
 @SpringBootApplication
 public class OnlineBankingApplication {
 
+    /**
+     * The application's entry point.
+     *
+     * @param args an array of command-line arguments for the application
+     */
     public static void main(String[] args) {
         SpringApplication.run(OnlineBankingApplication.class, args);
     }
