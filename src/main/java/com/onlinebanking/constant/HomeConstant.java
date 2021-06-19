ac7472fc1e9f7d56d3ba5850ef7c0d001e7cec15
Matt <mpuent18@gmail.com>
2021-06-18 21:53:45 -0400

    #ob-XGRjm627 - Fixing the unstaged changes

--- src/main/java/com/onlinebanking/constant/HomeConstant.java
+++ src/main/java/com/onlinebanking/constant/HomeConstant.java
@@ -1,2 +1,21 @@
-package com.onlinebanking.constant;public class HomeConstant {
+package com.onlinebanking.constant;
+
+/**
+ *
+ * @author Matthew Puentes
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class HomeConstant {
+
+    /**
+     * URL Mapping Constants.
+     */
+    public static final String INDEX_URL_MAPPING = "/";
+
+    /**
+     * View Name Constants.
+     */
+    public static final String INDEX_VIEW_NAME = "index";
 }
--- src/main/java/com/onlinebanking/constant/package-info.java
+++ src/main/java/com/onlinebanking/constant/package-info.java
@@ -1,1 +1,9 @@
 package com.onlinebanking.constant;
+
+/**
+ * This package will house all constants used in the application.
+ *
+ * @author Matthew Puentes
+ * @version 1.0
+ * @since 1.0
+ */
