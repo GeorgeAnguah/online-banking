1cc97672a7596ce990cc6e1a2c2820565679b985
GeorgeAnguah <Georgeanguah@yahoo.com>
2021-06-19 00:14:22 -0400

    Create index controller -ob-fkjN1tqI

---
+++ src/main/java/com/onlinebanking/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.onlinebanking.controller;
+
+import com.onlinebanking.constant.HomeConstant;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * HomeController class managers all incoming request to the home page.
+ *
+ * @author George on 6/19/2021
+ * @version 1.0
+ * @since 1.0
+ */
+@Controller
+@RequestMapping(HomeConstant.INDEX_URL_MAPPING)
+public class HomeController {
+
+    public String home(){
+        return HomeConstant.INDEX_VIEW_NAME;
+    }
+}
---
+++ src/main/java/com/onlinebanking/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package will hold all controllers in this application
+ *
+ * @author George on 6/18/2021
+ * @version 1.0
+ * @since 1.0
+ */
+package com.onlinebanking.controller;
