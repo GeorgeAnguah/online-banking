0a917f2875799dae9fc066dd699b0c249a3ac3b1
Matt <mpuent18@gmail.com>
2021-06-22 07:06:35 -0400

    #ob-hmkExtcp: Added application configuration class

---
+++ src/main/java/com/onlinebanking/config/ApplicationConfig.java
@@ -0,0 +1,15 @@
+package com.onlinebanking.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This class holds application configuration settings for this application.
+ *
+ * @author Matthew Puentes on 6/22/2021
+ * @version 1.0
+ * @since 1.0
+ */
+
+@Configuration
+public class ApplicationConfig {
+}
