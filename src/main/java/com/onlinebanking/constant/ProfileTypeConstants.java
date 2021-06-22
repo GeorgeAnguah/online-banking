27e21a0d11dd287c493bf88d33df9089db22da33
Matt <mpuent18@gmail.com>
2021-06-22 07:24:43 -0400

    #ob-h25ULZST: Added profile type constants class

---
+++ src/main/java/com/onlinebanking/constant/ProfileTypeConstants.java
@@ -0,0 +1,28 @@
+package com.onlinebanking.constant;
+
+/**
+ * This class holds all profile type constants used in the application.
+ *
+ * @author Matthew Puentes on 6/22/2021
+ * @version 1.0
+ * @since 1.0
+ */
+
+public final class ProfileTypeConstants {
+
+    /**
+     * Dev Profile Constant.
+     */
+    public static final String DEV = "dev";
+    /**
+     * Prod Profile Constant.
+     */
+    public static final String PROD = "prod";
+    /**
+     * Test Profile Constant.
+     */
+    public static final String TEST = "test";
+
+    private ProfileTypeConstants() {
+    }
+}
