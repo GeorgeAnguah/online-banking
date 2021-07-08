9ac754c97a0caaee643e2d329d0748db521adf58
Matt <mpuent18@gmail.com>
2021-07-08 11:53:33 -0400

    #ob-3y9ytZeU: Fixed errors with util class

--- src/main/java/com/onlinebanking/shared/UserUtils.java
+++ src/main/java/com/onlinebanking/shared/UserUtils.java
@@ -8,6 +8,13 @@ import com.onlinebanking.backend.persistent.domain.User;
 import java.text.MessageFormat;
 import java.util.Locale;
 
+/**
+ * User utility class that holds methods used across application.
+ *
+ * @author Matthew Puentes
+ * @version 1.0
+ * @since 1.0
+ */
 public final class UserUtils {
     private static final Faker FAKER = new Faker();
     private static final int MIN_LENGTH = 4;
@@ -16,6 +23,11 @@ public final class UserUtils {
     private UserUtils() {
     }
 
+    /**
+     * Create a user.
+     *
+     * @return a user
+     */
     public static User createUser() {
         return createUser(FAKER.name().username());
     }
@@ -23,17 +35,34 @@ public final class UserUtils {
     /**
      * Create a user with some flexibility.
      *
-     * @param username
-     * @return
+     * @param username username used to create user.
+     * @return a user
      */
     public static User createUser(String username) {
         return createUser(username, FAKER.internet().password(MIN_LENGTH, MAX_LENGTH), FAKER.internet().emailAddress());
     }
 
+    /**
+     * Create a user with some flexibility.
+     *
+     * @param username username used to create user
+     * @param password password used to create user.
+     * @param email email used to create user.
+     * @return a user
+     */
     public static User createUser(String username, String password, String email) {
         return createUser(username, password, email, false);
     }
 
+    /**
+     * Create user with some flexibility.
+     *
+     * @param username username used to create user.
+     * @param password password used to create user.
+     * @param email email used to create user.
+     * @param enabled boolean value used to evaluate if user enabled.
+     * @return a user
+     */
     public static User createUser(String username, String password, String email, boolean enabled) {
         User user = new User();
         user.setUsername(username);
@@ -50,10 +79,21 @@ public final class UserUtils {
         return user;
     }
 
+    /**
+     * Generate a public id for user.
+     *
+     * @return public id
+     */
     public static String generatePublicId() {
         return generatePublicId(30);
     }
 
+    /**
+     * Generate a public id for user.
+     *
+     * @param length length used for public id.
+     * @return public id
+     */
     public static String generatePublicId(int length) {
         FakeValuesService fakeValuesService = new FakeValuesService(Locale.ENGLISH, new RandomService());
         return fakeValuesService.regexify(MessageFormat.format("[A-Z1-9a-z]'{'{0}'}'", length));
--- src/main/java/com/onlinebanking/shared/package-info.java
+++ src/main/java/com/onlinebanking/shared/package-info.java
@@ -1,10 +1,4 @@
 /**
-<<<<<<< Updated upstream
  * Package containing shared resources within layers the application.
  */
 package com.onlinebanking.shared;
-=======
- * Package containing shared resources within layers of the application.
- */
-package com.onlinebanking.shared;
->>>>>>> Stashed changes
