2e880cb2b8e0a99a23a1737a59693505a9f5a914
Eric Opoku <eric.opoku20@yahoo.com>
2021-08-06 01:09:58 -0400

    Refactored role repository to use crud repository

--- src/main/java/com/onlinebanking/backend/persistent/repository/RoleRepository.java
+++ src/main/java/com/onlinebanking/backend/persistent/repository/RoleRepository.java
@@ -1,6 +1,7 @@
 package com.onlinebanking.backend.persistent.repository;
 
 import com.onlinebanking.backend.persistent.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
